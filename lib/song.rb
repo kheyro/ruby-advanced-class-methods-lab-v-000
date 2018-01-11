af5df3848af475d74560931b766d7bc0e45dc260
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 15:44:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,8 @@ class Song
   end
 
   def self.new_by_name(dname)
-    song = self.new
-    song.artist_name = dname
+    song = Song.new
+    song.name = dname
   end
 
   def self.create_by_name(name)

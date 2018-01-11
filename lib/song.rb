62cfa2c73b7c4274d81ac7964a548900cd8f31d9
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-11 16:37:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,6 +46,8 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    filename.split(/[-.\s]/)
-
+    file = filename.split(/[-.\s]/).reject!(&:empty?)
+    song = self.new
+    song.name = file[1]
+    song
 end
